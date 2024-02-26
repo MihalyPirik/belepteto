8763ef8e08f71f02195e35f23c884d2adac9a5ff
Mihaly Pirik <misipirik@gmail.com>
2024-02-26 09:39:18 +0100

    import modules and create login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app.component';
 
 import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule} from 'primeng/inputtext';
+import { PasswordModule} from 'primeng/password';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
 
     // primeng modulok
-    MenubarModule
+    MenubarModule,
+    InputTextModule,
+    PasswordModule
   ],
   providers: [],
   bootstrap: [AppComponent]
